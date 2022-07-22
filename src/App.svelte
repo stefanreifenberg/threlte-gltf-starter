eefedc99b44cbc6938fc7e53c3d0191edff7df46
Stefan Reifenberg <stefan.reifenberg@googlemail.com>
2022-07-22 09:16:53 +0200

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<script>
+  import { Canvas } from '@threlte/core'
+  import Model from './components/Model.svelte'
+  import {
+		AmbientLight,
+		OrbitControls,
+		PerspectiveCamera
+	} from '@threlte/core'
+</script>
+
+<div class="canvas-wrapper">
+
+  <Canvas>
+    <PerspectiveCamera fov={90} position={{ x: 0, y:0, z: -12 }}>
+      <OrbitControls  autoRotate autoRotateSpeed={0.8} />
+    </PerspectiveCamera>
+    <AmbientLight intensity={0.9}  />
+
+    <AmbientLight />  
+    <Model />
+  </Canvas>
+
+</div>
+
+
+<style>
+  .canvas-wrapper {
+    position: fixed;
+	  top: 0;
+	  left: 0;
+	  width: 100%;
+	  height: 100%;
+  }
+</style>
---
+++ src/components/Model.svelte
@@ -0,0 +1,6 @@
+<script>
+    import { GLTF } from '@threlte/extras'
+</script>
+
+<GLTF castShadow receiveShadow url={'/Duck.glb'} position={{ y: 0 }} scale={0.8} />
+
